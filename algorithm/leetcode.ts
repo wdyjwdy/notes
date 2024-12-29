0f91657c711750e3c2655b7c8f1afe7e6abd6b55
wdyjwdy <46589632+wdyjwdy@users.noreply.github.com>
2024-12-29 15:28:07 +0800

    Update leetcode.ts
    isPalindrome

--- algorithm/leetcode.ts
+++ algorithm/leetcode.ts
@@ -7,3 +7,15 @@ function twoSum(numbers: number[], target: number): number[] {
         else return [a + 1, b + 1]
     }
 };
+
+function isPalindrome(s: string): boolean {
+    let str = s.toLowerCase().replace(/[^0-9a-z]/g, '')
+
+    let [a, b] = [0, str.length - 1]
+    while (a < b) {
+        if (str[a] !== str[b]) return false
+        a++
+        b--
+    }
+    return true
+};
