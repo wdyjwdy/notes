01e2ee4ef432394c463a952b3f73a2e90634e8a7
wdyjwdy <46589632+wdyjwdy@users.noreply.github.com>
2025-01-04 16:27:34 +0800

    Update leetcode.ts
    rotate

--- algorithm/leetcode.ts
+++ algorithm/leetcode.ts
@@ -588,3 +588,17 @@ function productExceptSelf(nums: number[]): number[] {
     }
     return result
 };
+
+function rotate(matrix: number[][]): void {
+    let n = matrix.length
+    for (let i = 0; i < Math.floor(n / 2); i++) {
+        for (let j = 0; j < Math.ceil(n / 2); j++) {
+            console.log(i, j)
+            let tmp = matrix[i][j]
+            matrix[i][j] = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = tmp
+        }
+    }
+};
